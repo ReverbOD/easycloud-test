02141a0558982d76800afc0ac12e7a2bcc24063f
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-02-05 14:18:45 +0100

    setted base project
    Properly setted workspace with springboot.
    
    Used:
    Maven Project
    Java 15
    Spring Boot 2.4.2
    
    Setted with:
    - Spring Web
    - Spring Data JPA
    - Thymeleaf (Template Engine)
    - MySQL Driver

---
+++ src/main/java/com/registry/RegistryRest/RegistryRestApplication.java
@@ -0,0 +1,13 @@
+package com.registry.RegistryRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registry/RegistryRest/RegistryRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registry.RegistryRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
